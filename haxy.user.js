dcdfea7f7e3f59ca652930c7b93950b86718f452
razorapple <35845382+RazorApple@users.noreply.github.com>
2019-07-14 01:59:06 +0300

    hxrks

--- haxy.user.js
+++ haxy.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Haxy Krunker. Make u pro.
 // @version      1.3
-// @author       gpy-dev
+// @author       gpy-dev // hackers1337
 // @include      /^(https?:\/\/)?(www\.)?(.+)krunker\.io(|\/|\/\?(game|server|party)=.+)$/
 // @grant        GM_xmlhttpRequest
 // @run-at       document-start
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
-// @namespace gpy
+// @namespace gpy // hxors1337
 // ==/UserScript==
 
 window.stop();
@@ -34,9 +34,9 @@ GM_xmlhttpRequest({
                 let zip = res.responseText;
                 zip = zip.replace(/setInterval.*?\);/, '');
                 html = html.replace(/<script src="libs\/zip\.js.+"><\/script>/, `<script>${zip}</script>`);
-                html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/bypass.js"></script>';
-                html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/haxy.js"></script>';
-                html += '<script src="https://raw.githack.com/gpy-dev/krunker/master/game.js"></script>';
+                html += '<script src="https://raw.githack.com/hackers1337/krunker/master/bypass.js"></script>';
+                html += '<script src="https://raw.githack.com/hackers1337/krunker/master/haxy.js"></script>';
+                html += '<script src="https://raw.githack.com/hackers1337/krunker/master/game.js"></script>';
 
                 document.open();
                 document.write(html);
